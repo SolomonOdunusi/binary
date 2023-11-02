6fa763284a7b3ff2eded49e60a63bb3e348914b5
Solomon Odunusi <solomonodunsui@gmail.com>
2023-11-02 14:23:09 -0700

    Declared the variable at the start

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -21,10 +21,11 @@ int binary_tree_balance(const binary_tree_t *tree)
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
+
 	if (tree)
 	{
-		size_t left_height = 0, right_height = 0;
-
+		left_height = 0, right_height = 0;
 		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((left_height > right_height) ? left_height : right_height);
